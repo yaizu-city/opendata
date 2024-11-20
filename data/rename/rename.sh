b83b42d144c4282aade6862cdbca53b33474ac7d
naoki <n.globe.us@gmail.com>
2024-11-21 04:51:01 +0900

    不要なファイルを削除

--- data/rename/rename.sh
+++
@@ -1,6 +0,0 @@
-find . -type f -name '*EPSG6676.*' | while read filepath; do
-  dirname=$(dirname "$filepath")
-  filename=$(basename "$filepath")
-  newname=$(echo "$filename" | sed 's/EPSG6676//')
-  mv "$filepath" "$dirname/$newname"
-done
