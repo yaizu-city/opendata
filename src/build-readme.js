dd29f615d302209cb108d89633fd40c7546a2134
naoki <n.globe.us@gmail.com>
2025-03-05 16:41:54 +0900

    fix build-readme

--- src/build-readme.js
+++ src/build-readme.js
@@ -2,15 +2,8 @@ const fs = require('fs');
 const glob = require('glob');
 const path = require('path');
 
-let locationDataCategories = [];
-if (fs.existsSync('./location-data-categories.json')) {
-  locationDataCategories = require('./location-data-categories.json');
-}
-
-let standardDataCategories = [];
-if (fs.existsSync('./standard-data-categories.json')) {
-  standardDataCategories = require('./standard-data-categories.json');
-}
+const locationDataCategoriesPath = path.resolve(__dirname, 'location-data-categories.json');
+const standardDataCategoriesPath = path.resolve(__dirname, 'standard-data-categories.json');
 
 class BuildReadme {
   run() {
@@ -20,21 +13,25 @@ class BuildReadme {
     readme += "焼津市では、以下のデータをオープンデータとして提供しています。\n\n表内の **「CSV」** や **「GeoJSON」** 、 **「JSON」** をクリックすると、最新の該当データが得られます。\n\n「フォルダ」には、xlsxファイルが格納されています。\n\nCSVファイルダウンロード時、文字化けする場合は、xlsxファイルをダウンロードしてください。\n\n位置情報を含むデータを編集する場合には、**編集**リンクをクリックします。データが地図上に表示され、表組み形式でデータを編集し、編集済みデータをダウンロードすることができます。\n\nデータが誤っている、追加したい、等のご提案には、編集済みのデータをプルリクエストとして送ってください。焼津市役所で確認の上、取り込みさせていただきます。詳しくは [焼津市オープンデータへの貢献方法](CONTRIBUTING.md) を参照してください。\n\n";
     readme += "| データ名 | フォルダ | CSV | GeoJSON | 地図で編集 |\n";
     readme += "| --- | --- | --- | --- | --- |\n";
-    
-    // locationDataCategories が存在する場合のみ処理
-    if (locationDataCategories.length > 0) {
-      for (let i = 0; i < locationDataCategories.length; i++) {
-        const category = locationDataCategories[i];
-        const csvFile = glob.sync(`data/${category.category}/*.csv`)[0];
-        const csvFolderUrl = `https://github.com/yaizu-city/opendata/tree/main/data/${category.category}`;
-        const csvFileUrl = `https://yaizu-smartcity.jp/${category.category}/data.csv`;
-        const jsonFileUrl = `https://yaizu-smartcity.jp/${category.category}/data.geojson`;
-        const mapUrl = `${opendataViewerUrl}?data=${csvFileUrl}`;
 
-        if (!csvFile || path.basename(csvFile) === "attributes.csv") {
-          readme += `| ${category.name} | [フォルダ](${csvFolderUrl}) | | [GeoJSON](${jsonFileUrl}) | |\n`;
-        } else {
-          readme += `| ${category.name} | [フォルダ](${csvFolderUrl}) | [CSV](${csvFileUrl}) | [GeoJSON](${jsonFileUrl}) | [編集](${mapUrl}) |\n`;
+    if (fs.existsSync(locationDataCategoriesPath)) {
+
+      const locationDataCategories = JSON.parse(locationDataCategoriesPath);
+      // locationDataCategories が存在する場合のみ処理
+      if (locationDataCategories.length > 0) {
+        for (let i = 0; i < locationDataCategories.length; i++) {
+          const category = locationDataCategories[i];
+          const csvFile = glob.sync(`data/${category.category}/*.csv`)[0];
+          const csvFolderUrl = `https://github.com/yaizu-city/opendata/tree/main/data/${category.category}`;
+          const csvFileUrl = `https://yaizu-smartcity.jp/${category.category}/data.csv`;
+          const jsonFileUrl = `https://yaizu-smartcity.jp/${category.category}/data.geojson`;
+          const mapUrl = `${opendataViewerUrl}?data=${csvFileUrl}`;
+  
+          if (!csvFile || path.basename(csvFile) === "attributes.csv") {
+            readme += `| ${category.name} | [フォルダ](${csvFolderUrl}) | | [GeoJSON](${jsonFileUrl}) | |\n`;
+          } else {
+            readme += `| ${category.name} | [フォルダ](${csvFolderUrl}) | [CSV](${csvFileUrl}) | [GeoJSON](${jsonFileUrl}) | [編集](${mapUrl}) |\n`;
+          }
         }
       }
     }
@@ -43,34 +40,39 @@ class BuildReadme {
     readme += "| データ名 | フォルダ | CSV | JSON |\n";
     readme += "| --- | --- | --- | --- |\n";
 
-    // standardDataCategories が存在する場合のみ処理
-    if (standardDataCategories.length > 0) {
-      for (let i = 0; i < standardDataCategories.length; i++) {
-        const category = standardDataCategories[i];
-        const csvFolderUrl = `https://github.com/yaizu-city/opendata/tree/main/data/${category.category}`;
-        const csvFileUrl = `https://yaizu-smartcity.jp/${category.category}/data.csv`;
-        const jsonFileUrl = `https://yaizu-smartcity.jp/${category.category}/data.json`;
-        
-        if (category.category === "city_planning_basic_survey_information") {
-          const csvFiles = glob.sync(`data/${category.category}/*.csv`);
-          const xlsxFiles = glob.sync(`data/${category.category}/*.xlsx`);
-          const mixedFiles = csvFiles.concat(xlsxFiles);
-          const allFileNames = [...new Set(mixedFiles.map(file => path.basename(file, path.extname(file))))];
+    if (fs.existsSync(standardDataCategoriesPath)) {
 
-          allFileNames.map(filename => {
-            const jsonFileUrl = `https://yaizu-smartcity.jp/${category.category}/${filename}.json`;
-            const csvFileUrl = `https://yaizu-smartcity.jp/${category.category}/${filename}.csv`;
-            const subCategory = filename.split('_')[1];
-            if (filename === allFileNames[0]) {
-              readme += `| ${category.name} | [フォルダ](${csvFolderUrl}) | [CSV(${subCategory})](${csvFileUrl}) | [JSON(${subCategory})](${jsonFileUrl}) |\n`;
-            } else {
-              readme += `||| [CSV(${subCategory})](${csvFileUrl}) | [JSON(${subCategory})](${jsonFileUrl}) |\n`;
-            }
-          });
-        } else if (category.historical) {
-          readme += `| ${category.name} | [フォルダ](${csvFolderUrl}) | [CSV(最新データ)](${csvFileUrl}) | [JSON(最新データ)](${jsonFileUrl}) |\n`;
-        } else {
-          readme += `| ${category.name} | [フォルダ](${csvFolderUrl}) | [CSV](${csvFileUrl}) | [JSON](${jsonFileUrl}) |\n`;
+      const standardDataCategories = JSON.parse(standardDataCategoriesPath);
+
+      // standardDataCategories が存在する場合のみ処理
+      if (standardDataCategories.length > 0) {
+        for (let i = 0; i < standardDataCategories.length; i++) {
+          const category = standardDataCategories[i];
+          const csvFolderUrl = `https://github.com/yaizu-city/opendata/tree/main/data/${category.category}`;
+          const csvFileUrl = `https://yaizu-smartcity.jp/${category.category}/data.csv`;
+          const jsonFileUrl = `https://yaizu-smartcity.jp/${category.category}/data.json`;
+
+          if (category.category === "city_planning_basic_survey_information") {
+            const csvFiles = glob.sync(`data/${category.category}/*.csv`);
+            const xlsxFiles = glob.sync(`data/${category.category}/*.xlsx`);
+            const mixedFiles = csvFiles.concat(xlsxFiles);
+            const allFileNames = [...new Set(mixedFiles.map(file => path.basename(file, path.extname(file))))];
+
+            allFileNames.map(filename => {
+              const jsonFileUrl = `https://yaizu-smartcity.jp/${category.category}/${filename}.json`;
+              const csvFileUrl = `https://yaizu-smartcity.jp/${category.category}/${filename}.csv`;
+              const subCategory = filename.split('_')[1];
+              if (filename === allFileNames[0]) {
+                readme += `| ${category.name} | [フォルダ](${csvFolderUrl}) | [CSV(${subCategory})](${csvFileUrl}) | [JSON(${subCategory})](${jsonFileUrl}) |\n`;
+              } else {
+                readme += `||| [CSV(${subCategory})](${csvFileUrl}) | [JSON(${subCategory})](${jsonFileUrl}) |\n`;
+              }
+            });
+          } else if (category.historical) {
+            readme += `| ${category.name} | [フォルダ](${csvFolderUrl}) | [CSV(最新データ)](${csvFileUrl}) | [JSON(最新データ)](${jsonFileUrl}) |\n`;
+          } else {
+            readme += `| ${category.name} | [フォルダ](${csvFolderUrl}) | [CSV](${csvFileUrl}) | [JSON](${jsonFileUrl}) |\n`;
+          }
         }
       }
     }
