06b636346321f673a9315d89bf8761640d56b8c6
naoki <n.globe.us@gmail.com>
2025-03-12 09:27:06 +0900

    some fix

--- src/build-readme.js
+++ src/build-readme.js
@@ -9,7 +9,7 @@ class BuildReadme {
   run() {
     const opendataViewerUrl = "https://geolonia.github.io/opendata-editor/";
     let readme = "# 焼津オープンデータカタログ\n\n";
-    readme += "焼津市では、以下のデータをオープンデータとして提供しています\n\nご利用にあたっては、[焼津市オープンデータカタログ利用規約](https://yaizu-smartcity.jp/%E7%84%BC%E6%B4%A5%E3%82%AA%E3%83%BC%E3%83%95%E3%82%9A%E3%83%B3%E3%83%86%E3%82%99%E3%83%BC%E3%82%BF%E3%82%AB%E3%82%BF%E3%83%AD%E3%82%AF%E3%82%99%E5%88%A9%E7%94%A8%E8%A6%8F%E7%B4%8420250306.pdf) をご確認ください。\n\n";
+    readme += "焼津市では、以下の地図データをオープンデータとして提供しています。\n\nご利用にあたっては、[焼津市オープンデータカタログ利用規約](https://yaizu-smartcity.jp/%E7%84%BC%E6%B4%A5%E3%82%AA%E3%83%BC%E3%83%95%E3%82%9A%E3%83%B3%E3%83%86%E3%82%99%E3%83%BC%E3%82%BF%E3%82%AB%E3%82%BF%E3%83%AD%E3%82%AF%E3%82%99%E5%88%A9%E7%94%A8%E8%A6%8F%E7%B4%8420250306.pdf) をご確認ください。\n\n";
     readme += "[本データ](https://github.com/yaizu-city/opendata/tree/main/data)は、[公共データ利用規約（第1.0版）](https://www.digital.go.jp/resources/open_data/public_data_license_v1.0)の下に提供されています。\n\n";
     readme += "なお、このリポジトリに含まれるソースコードは、上記ライセンスの適用外です。\n\n";
     readme += "## ▼焼津オープンデータカタログの使い方\n\n";
